495a5947299497a83898e4816d7ef094075a4acc
mkshin96 <tlsanrhs96@gmail.com>
2022-01-10 22:54:06 +0900

    feat: airbnb MongoDB module

---
+++ src/main/java/clone/airbnbmongo/AirbnbMongoApplication.java
@@ -0,0 +1,13 @@
+package clone.airbnbmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbnbMongoApplication.class, args);
+    }
+
+}
