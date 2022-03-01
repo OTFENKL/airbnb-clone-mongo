5add10546c4abde964dea648e2bd99e5253e1aea
kiseok, Yang <rltjr219@gmail.com>
2022-03-01 18:34:18 +0900

    ci: deploy scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/airbnb-mongo
+cd $REPOSITORY
+
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_FILE=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할것 없음."
+else
+  echo "> kill -9 $CURRENT_PID"
+  source ./stop.sh
+fi
+
+echo "> $JAR_FILE 배포"
+source ./start.sh
