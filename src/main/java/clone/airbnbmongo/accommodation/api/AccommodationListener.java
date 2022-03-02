cd0ed061da3d88ff69df6eeefe508b121bc2c637
kiseok, Yang <rltjr219@gmail.com>
2022-03-03 00:05:08 +0900

    chore: annotate rabbitmq

--- src/main/java/clone/airbnbmongo/accommodation/api/AccommodationListener.java
+++ src/main/java/clone/airbnbmongo/accommodation/api/AccommodationListener.java
@@ -13,7 +13,7 @@ public class AccommodationListener {
     private final AccommodationService accommodationService;
     private final QueueConverter queueConverter;
 
-    @RabbitListener(queues = {"airbnb-clone"})
+//    @RabbitListener(queues = {"airbnb-clone"})
     public <T> void receiveAccommodation(String data)  {
         AccommodationQueueRes accommodationQueueRes = queueConverter.convert(data, AccommodationQueueRes.class);
         accommodationService.createAccommodation(accommodationQueueRes);
