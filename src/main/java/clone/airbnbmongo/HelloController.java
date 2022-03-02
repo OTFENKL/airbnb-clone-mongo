2fd2bdb7f30426e5e7e3f4e2137cfc677df376c0
kiseok, Yang <rltjr219@gmail.com>
2022-03-02 22:41:54 +0900

    chore: test api

---
+++ src/main/java/clone/airbnbmongo/HelloController.java
@@ -0,0 +1,14 @@
+package clone.airbnbmongo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello()   {
+        return "Hello, Airbnb Mongo!";
+    }
+
+}
