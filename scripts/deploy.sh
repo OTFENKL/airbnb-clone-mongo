9583effd6a0274db7390a1742a09554681768966
kiseok, Yang <rltjr219@gmail.com>
2022-03-02 22:14:28 +0900

    ci: appspec.yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/airbnb-mongo
-SCRIPT_DIR=$(dirs)
+APP_DIR=/home/ec2-user/app/airbnb-mongo/archive-jar
 
-cd $REPOSITORY
-
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_FILE=$REPOSITORY/build/libs/$JAR_NAME
-
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+APP_NAME=$(ls APP_DIR | grep .jar)
+CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 종료할것 없음."
+  echo "> 종료할 APP이 없습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
-  $SCRIPT_DIR/stop.sh
+  $APP_DIR/stop.sh
 fi
 
-echo "> $JAR_FILE 배포"
-$SCRIPT_DIR/start.sh
+$APP_DIR/start.sh
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-kill -15 $CURRENT_PID
+echo "> kill -15 ${CURRENT_PID}"
+kill -15 ${CURRENT_PID}
+echo "> ${CURRENT_PID} 삭제 완료"
